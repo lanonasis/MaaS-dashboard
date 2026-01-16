01d0bce90b476ea006bfcc858fe4149801c3d64b
The Fixer <info@lanonasis.com>
2026-01-16 04:16:44 +0100

    feat: add test configuration and improve type definitions for Vitest
    Signed-off-by: The Fixer <info@lanonasis.com>

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+/// <reference types="vitest/globals" />
+/// <reference types="@testing-library/jest-dom" />
+
+import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
+
+declare module 'vitest' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    interface Assertion<T = any> extends TestingLibraryMatchers<typeof expect.stringContaining, T> { }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    interface AsymmetricMatchersContaining extends TestingLibraryMatchers<typeof expect.stringContaining, any> { }
+}
